65a3e37af09b7486cc24fb7649d6f0167a2a4a18
goooochi <yuki.yamaguchi@ksf.biglobe.ne.jp>
2022-03-15 11:56:30 +0900

    test

--- View/Controller/SecondViewController/AlbumViewController.swift
+++ View/Controller/SecondViewController/AlbumViewController.swift
@@ -14,6 +14,9 @@ class AlbumViewController: UIViewController ,UITextFieldDelegate,UIImagePickerCo
     
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var commentString: UITextView!
+    
+    
     
 
     override func viewDidLoad() {
@@ -26,16 +29,54 @@ class AlbumViewController: UIViewController ,UITextFieldDelegate,UIImagePickerCo
     
     @IBAction func selectAlbum(_ sender: Any) {
         
+        //UIImagePickerController.SourceTypeという型
+        let sourceType:UIImagePickerController.SourceType = .photoLibrary
+        //メソッドcreateImagePickerが呼ばれるx
+        createImagePicker(sourceType: sourceType)
+        
         
     }
+    
+    
+    func createImagePicker(sourseType:UIImagePickerController.SourceType){
+        
+        
+        
+        //インスタンスを作成
+        let cameraPicker = UIImagePickerController()
+        
+        cameraPicker.sourceType = sourseType
+        
+        //cameraPickerに使えるデリゲートメソッドをViewControllerでも使えるようにする
+        //他のクラスから自分のクラスへ委任を受ける
+        cameraPicker.delegate = self
+        
+        cameraPicker.allowsEditing = true
+        
+        self.present(cameraPicker, animated: true, completion: nil)
+        
+        
+        
+    }
+    
     
     
     @IBAction func next(_ sender: Any) {
         
+        performSegue(withIdentifier: "next", sender: nil)
         
     }
     
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let shareVC = segue.destination as! ShareViewController
+        
+        shareVC.commentString = commentString.text
+        
+        shareVC.resultImage = imageView.image!
+        
+    }
     
 }
+    
